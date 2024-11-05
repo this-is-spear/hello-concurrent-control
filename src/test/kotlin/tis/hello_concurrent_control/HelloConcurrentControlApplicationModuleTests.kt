9d66150b8fec82cb28da4692971bf26984dcc8a7
this-is-spear <92219795+this-is-spear@users.noreply.github.com>
2024-11-06 01:57:22 +0900

    test : 모듈 구성 정보 문서 조회 테스트 추가

--- src/test/kotlin/tis/hello_concurrent_control/HelloConcurrentControlApplicationModuleTests.kt
+++ src/test/kotlin/tis/hello_concurrent_control/HelloConcurrentControlApplicationModuleTests.kt
@@ -2,6 +2,7 @@ package tis.hello_concurrent_control
 
 import org.junit.jupiter.api.Test
 import org.springframework.modulith.core.ApplicationModules
+import org.springframework.modulith.docs.Documenter
 
 
 class HelloConcurrentControlApplicationModuleTests {
@@ -9,5 +10,16 @@ class HelloConcurrentControlApplicationModuleTests {
     fun verifyModularity() {
         val modules = ApplicationModules.of(HelloConcurrentControlApplication::class.java)
         modules.verify()
+    }
+
+    /**
+     * build/spring-modulith-docs contains the generated documentation.
+     */
+    @Test
+    fun createModuleDocumentation() {
+        val modules = ApplicationModules.of(HelloConcurrentControlApplication::class.java)
+        Documenter(modules)
+            .writeDocumentation()
+            .writeIndividualModulesAsPlantUml()
     }
 }
